4a25a51f2b341d9d92554d9f602cec7c8085a182
abdulazizrufatov <arufatov7@gmail.com>
2022-05-20 18:41:02 +0500

    initial commit

---
+++ js/indexs.js
@@ -0,0 +1,16 @@
+var username = prompt("ismingiz", "Abdulaziz");
+var userfirstName = prompt("familyangiz" , "Rufatov");
+var userage = prompt("yoshingiz", "17");
+var userfrom = prompt("mamlakat" , "Uzbekiston");
+var usercity = prompt("shahar" , "Toshkent");
+var userjob = prompt("kasbingiz" , "It");
+var userstudy = prompt("qayerda o'qiysiz", "najot talim");
+
+
+console.log(username);
+console.log(userfirstName);
+console.log(userage);
+console.log(userfrom);
+console.log(usercity);
+console.log(userjob);
+console.log(userstudy);
